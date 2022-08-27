278f13b76a8fe45af92bcb6e44b88fa21b422952
Maryna Kernychna <marynakernychna@gmail.com>
2022-08-27 21:41:54 +0300

    Create the endpoint to get available to pick orders

---
+++ Backend/Core/DTO/Order/OrderInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.DTO.Order
+{
+    public class OrderInfoDTO
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string ClientFullName { get; set; }
+        public string ClientPhoneNumber { get; set; }
+        public int WaresCount { get; set; }
+    }
+}
---
+++ Backend/Core/Specifications/OrderSpecification.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.Specification;
+using Core.DTO;
+using Core.Entities;
+using System.Linq;
+
+namespace Core.Specifications
+{
+    public static class OrderSpecification
+    {
+        internal class GetAvailable : Specification<Order>
+        {
+            public GetAvailable(PaginationFilterDTO paginationFilterDTO)
+            {
+                Query.Where(o => o.CourierId == null)
+                     .Include(o => o.Cart)
+                     .ThenInclude(c => c.Creator)
+                     .Include(o => o.Cart)
+                     .ThenInclude(c => c.WareCarts)
+                     .Skip((paginationFilterDTO.PageNumber - 1) * paginationFilterDTO.PageSize)
+                     .Take(paginationFilterDTO.PageSize)
+                     .AsNoTracking();
+            }
+        }
+    }
+}
